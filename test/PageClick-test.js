d855c11a138e15dde2582319c3f8ccc443910f7a
Nik Butenko <nik@butenko.me>
2015-09-10 16:24:14 +1000

    Add tests for document click sub/unsub

--- test/PageClick-test.js
+++ test/PageClick-test.js
@@ -1,3 +1,7 @@
+import React from 'react/addons';
+const TestUtils = React.addons.TestUtils;
+
+
 describe('PageClick', () => {
   const PageClickInjector = require('inject!../src/PageClick');
   let mock, PageClick;
@@ -15,5 +19,40 @@ describe('PageClick', () => {
 
   it('should be ok', () => {
     expect(PageClick).toBeTruthy();
+  });
+
+
+
+  it('should require the only child to be present', () => {
+    expect(() => TestUtils.renderIntoDocument(<PageClick />))
+      .toThrow();
+  });
+
+
+  describe('Subscribe to document clicks', () => {
+    beforeEach(() => {
+      spyOn(global.window, 'addEventListener');
+      spyOn(global.window, 'removeEventListener');
+    });
+
+
+    it('should subscribe to mousedown on render', () => {
+      const pageClick = TestUtils.renderIntoDocument(<PageClick><span>Test</span></PageClick>);
+      expect(global.window.addEventListener).toHaveBeenCalled();
+      expect(global.window.addEventListener.calls.mostRecent().args[0]).toEqual('mousedown');
+    })
+
+
+    it('should unsubscribe on destroy', () => {
+      const div = document.createElement('div');
+      const pageClick = React.render(<PageClick><span>Test</span></PageClick>, div);
+      const onMouseDown = global.window.addEventListener.calls.mostRecent().args[1];
+      React.unmountComponentAtNode(div);
+
+      expect(global.window.removeEventListener).toHaveBeenCalled();
+      expect(global.window.removeEventListener.calls.mostRecent().args[0]).toEqual('mousedown');
+      expect(global.window.removeEventListener.calls.mostRecent().args[1])
+        .toEqual(onMouseDown);
+    })
   });
 });
